c7498a5614f37135d7a3918883978bfacef35492
Mário Guimarães <marioguima@hotmail.com>
2020-11-24 18:16:09 -0300

    Gravando as imagens em um subdiretorio 'participants'

--- app/Http/Livewire/User/UploadPhoto.php
+++ app/Http/Livewire/User/UploadPhoto.php
@@ -62,7 +62,7 @@ class UploadPhoto extends Component
         preg_match("/data:image\/(.*?);/", $image, $image_extension); // extract the image extension
         $image = preg_replace('/data:image\/(.*?);base64,/', '', $image); // remove the type part
         $image = str_replace(' ', '+', $image);
-        $imageName = 'participant_' . time() . '_' . \Str::random(5) . '.' . $image_extension[1]; //generating unique file name;
-        Storage::disk('public')->put($imageName, base64_decode($image));
+        $imageName = time() . '_' . \Str::random(5) . '.' . $image_extension[1]; //generating unique file name;
+        Storage::disk('public')->put('participants/'.$imageName, base64_decode($image));
     }
 }
